f05c0ea6afd91d4bdebad60181fc58dbe0bd4c04
ernesto1718 <erodassotomayor@gmail.com>
2026-04-13 08:00:46 -0500

    chore(app): register pinia, vue-router, and global css in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router/index.js'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@/assets/styles/main.css'
+import '@/assets/styles/utilities.css'
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
